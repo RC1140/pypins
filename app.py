3b32af8b784b86d37240cbeecdb97527d6604934
Jameel Haffejee <jameel@chasesoftware.co.za>
2016-01-19 20:52:53 +0200

    Minimal caching

--- app.py
+++ app.py
@@ -3,12 +3,23 @@ from slacker import Slacker
 import os
 
 app = Flask(__name__)
+slack = Slacker(os.getenv('SLACK_TOKEN', ''))
+users = {}
+
+def setup():
+    users =  slack.users.list().body['members']
+    userLookup = {}
+    for u in users:
+        userLookup[u['id']] = u['name']
+    return userLookup
 
 @app.route("/")
-def index():
-    slack = Slacker(os.getenv('SLACK_TOKEN', ''))
+def index():    
+    if len(users.keys()) == 0:
+        users = setup()
     response = slack.pins.list(os.getenv('SLACK_ROOM', ''))
-    return render_template('index.html',lines=response.body['items'],sl=slack)
+    return render_template('index.html',lines=response.body['items'],users=users)
 
 if __name__ == "__main__":
+    users = setup()
     app.run()
