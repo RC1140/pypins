4d66527bc40c2384b331b829ff3e6ca36dfcbb83
Jameel Haffejee <jameel@chasesoftware.co.za>
2016-01-22 08:51:25 +0200

    Added procfile changed cache to use flasks g variable

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from slacker import Slacker
 import os
 
 app = Flask(__name__)
+app.debug = True
 slack = Slacker(os.getenv('SLACK_TOKEN', ''))
-users = {}
+
+@app.before_request
+def initialUserLoad():
+    if not g.get('users',None):
+        g.users = setup()
 
 def setup():
-    users =  slack.users.list().body['members']
+    immutableUsers =  slack.users.list().body['members']
     userLookup = {}
-    for u in users:
+    for u in immutableUsers:
         userLookup[u['id']] = u['name']
     return userLookup
 
 @app.route("/")
 def index():    
-    if len(users.keys()) == 0:
-        users = setup()
     response = slack.pins.list(os.getenv('SLACK_ROOM', ''))
-    return render_template('index.html',lines=response.body['items'],users=users)
+    return render_template('index.html',lines=response.body['items'],users=g.users)
 
 if __name__ == "__main__":
     users = setup()
